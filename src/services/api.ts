e2974d268fafe7f2c2a300003a87f861748e1329
m4rc1an0 <marciano.arantes1999@gmail.com>
2023-02-07 12:20:32 -0300

    Finalizando Projeto

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const baseURL = 'https://api.github.com/users/'
 
+const token_git = 'Bearer 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'
 
 const api = axios.create({
     baseURL: baseURL,
+    headers: {
+        Authorization: token_git
+    }
 })
 
 export default api
